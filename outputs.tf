6c660022d61eda8e9a32e3859bc315896f4bc85a
Matt Bernard <matt.bernard@nearform.com>
2020-10-20 13:34:43 -0700

    add a CI user, remove unused outputs

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,9 @@
-output "client_key" {
-    value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_key
-}
-
-output "client_certificate" {
-    value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate
-}
-
-output "cluster_ca_certificate" {
-    value = azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate
-}
-
 output "cluster_username" {
     value = azurerm_kubernetes_cluster.k8s.kube_config.0.username
 }
-
 output "cluster_password" {
     value = azurerm_kubernetes_cluster.k8s.kube_config.0.password
 }
-
-output "kube_config" {
-    value = azurerm_kubernetes_cluster.k8s.kube_config_raw
-}
-
 output "host" {
     value = azurerm_kubernetes_cluster.k8s.kube_config.0.host
 }
@@ -33,4 +15,14 @@ output "cluster_egress_ip" {
 }
 output "keyvault_id" {
   value = azurerm_key_vault.kv.id
+}
+output "ci_display_name" {
+  value = azuread_service_principal.ci-user.display_name
+}
+output "ci_client_id" {
+  value = azuread_application.app.application_id
+}
+output "ci_client_secret" {
+  value     = azuread_application_password.ci-user.value
+  sensitive = true
 }
